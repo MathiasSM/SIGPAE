f95bf464c0b111be4d097ba18c7aa944ad29e857
pablodbc <pablodbc30@gmail.com>
2017-03-17 02:48:08 -0400

    Al fin encontré validadores serios, on it

--- ocr/static/js/validateFields.js
+++ ocr/static/js/validateFields.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
-	$("#fecha_año").attr({
-		"min" : 1969,
-		"max" : ((new Date).getFullYear() + 1)
-	})
+    $("#fecha_año").attr({
+        "min" : 1969,
+        "max" : ((new Date).getFullYear() + 1)
+    })
+    $("#fecha_año").on('change invalid', function() {
+        var textfield = $(this).get(0);
+        textfield.setCustomValidity('');
+        if (textfield.validity.rangeUnderflow) {
+            textfield.setCustomValidity('El año ingresado debe ser mayor a 1969');
+        }
+        if (textfield.validity.rangeOverflow) {
+            textfield.setCustomValidity('El año ingresado debe ser menor a ' + ((new Date).getFullYear() + 1));
+        }
+
+        if (textfield.validity.valueMissing) {
+          textfield.setCustomValidity('Este campo es obligatorio');  
+        }
+    })
 })

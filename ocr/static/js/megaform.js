2aed2550ff132c0a19855c92f1f364415aeb5870
ahidalgob <ahidalgo94@gmail.com>
2017-03-31 04:06:50 -0400

    Refactor de las funciones del megaform

--- ocr/static/js/megaform.js
+++ ocr/static/js/megaform.js
@@ -21,14 +21,59 @@ $(document).ready(function () {
     }
   }
 
+  function anadirUnAutor(num1, num2){
+    var autorCNT = "#autorCNT" + num1 + '-' + num2;
+    autorCount[num1-1][num2-1]++;
+    var num3 = autorCount[num1-1][num2-1];
+
+    $(autorCNT).append(
+      '<hr>\
+      <div id="autor'+num1+'-'+num2+'-'+num3+'">\
+      <label><small>Autor '+num1+'.'+num2+'.'+num3+'</small></label>\
+      <input type="hidden" name="autorpk'+num1+'-'+num2+'-'+num3+'" value=-1>\
+      <input type="text" name="nombres'+num1+'-'+num2+'-'+num3+'" placeholder="Nombres"">\
+      <input type="text" name="apellidos'+num1+'-'+num2+'-'+num3+'" placeholder="Apellidos"">\
+      </div>');
+  }
+
+  function anadirUnaReferencia(num1){
+    var referenciaCNT = "#referenciaCNT" + num1;       // Generar el identificador al contenedor de la referencia correspondiente
+
+    referenciaCount[num1-1]++;                         // Aumentar el contador de phones para el referencia permitente
+    var num2 = referenciaCount[num1-1];                // Variable auxiliar para la sustitucion en el html de abajo
+
+    autorCount[num1-1] = autorCount[num1-1].concat([0]);
+
+    // Inserción del html
+    $(referenciaCNT).append(
+    '<hr>\
+    <div id="referencia'+num1+'-'+num2+'">\
+      <label><small>Referencia '+num1+'.'+num2+'</small></label>\
+      <input type="hidden" name="referenciapk'+num1+'-'+num2+'" value=-1>\
+      <input type="text" name="titulo'+num1+'-'+num2+'" placeholder="Título de la referencia">\
+      <div id="autorCNT'+num1+'-'+num2+'">\
+      </div>\
+      <div align="right">\
+        <button id="agregarAutor'+num1+'-'+num2+'" class="agregarAutor" type="button" style="float: right;"><small>Agregar autor</small></button>\
+      </div>\
+      </br>\
+      </br>\
+      <label><small>Editorial</small></label>\
+      <input type="text" name="editorial'+num1+'-'+num2+'" placeholder="Editorial de la referencia">\
+      <label><small>Edición</small></label>\
+      <input type="text" name="edicion'+num1+'-'+num2+'" placeholder="Edición de la referencia">\
+      <label><small>Notas</small></label>\
+      <input type="text" name="notas'+num1+'-'+num2+'" placeholder="Notas de la referencia">\
+    </div>');
+
+    anadirUnAutor(num1, num2);
+  }
+
   function anadirUnaSeccion(){
     seccionCount++;                                 // Aumentar el contador de secciones
-    referenciaCount = referenciaCount.concat([1]);  // Agregar un nuevo slot contador de referencias
-    autorCount = autorCount.concat([[1]]);
-
+    referenciaCount = referenciaCount.concat([0]);  // Agregar un nuevo slot contador de referencias
+    autorCount = autorCount.concat([[]]);
     var num1 = seccionCount;                        // Variable auxiliar para la sustitucion en el html de abajo
-    var num2 = referenciaCount[num1-1];             // Variable auxiliar para la sustitucion en el html de abajo
-    var num3 = autorCount[num1-1][num2-1];
 
     // Inserción del html
     $(seccionCNT).append(
@@ -39,30 +84,6 @@ $(document).ready(function () {
       <input type="text" name="seccionNombre'+num1+'" placeholder="Nombre de la sección"">\
       <div align="left"><label>Referencias</label></div>\
       <div id="referenciaCNT'+num1+'">\
-        <div id="referencia'+num1+'-'+num2+'">\
-          <label><small>Referencia '+num1+'.'+num2+'</small></label>\
-          <input type="hidden" name="referenciapk'+num1+'-'+num2+'" value=-1>\
-          <input type="text" name="titulo'+num1+'-'+num2+'" placeholder="Título de la referencia"">\
-          <div id="autorCNT'+num1+'-'+num2+'">\
-            <div id="autor'+num1+'-'+num2+'-'+num3+'">\
-              <label><small>Autor '+num1+'.'+num2+'.'+num3+'</small></label>\
-              <input type="hidden" name="autorpk'+num1+'-'+num2+'-'+num3+'" value=-1>\
-              <input type="text" name="nombres'+num1+'-'+num2+'-'+num3+'" placeholder="Nombres"">\
-              <input type="text" name="apellidos'+num1+'-'+num2+'-'+num3+'" placeholder="Apellidos"">\
-            </div>\
-          </div>\
-          <div align="right">\
-            <button id="agregarAutor'+num1+'-'+num2+'" class="agregarAutor" type="button" style="float: right;"><small>Agregar autor</small></button>\
-          </div>\
-        </br>\
-        </br>\
-          <label><small>Editorial</small></label>\
-          <input type="text" name="editorial'+num1+'-'+num2+'" placeholder="Editorial de la referencia"">\
-          <label><small>Edición</small></label>\
-          <input type="text" name="edicion'+num1+'-'+num2+'" placeholder="Edición de la referencia"">\
-          <label><small>Notas</small></label>\
-          <input type="text" name="notas'+num1+'-'+num2+'" placeholder="Notas de la referencia"">\
-        </div>\
       </div>\
       <div align="right">\
         <button id="agregarReferencia'+num1+'" class="agregarReferencia" type="button" style="float: right;"><small>Agregar referencia</small></button>\
@@ -70,7 +91,11 @@ $(document).ready(function () {
     </br>\
     </br>\
     </div>');
+
+    anadirUnaReferencia(num1);
   }
+
+
 
   if(seccionCount == 0) anadirUnaSeccion();
 
@@ -114,63 +139,16 @@ $(document).ready(function () {
   // Función para los botones para añadir referencias
   $("body").on("click","button.agregarReferencia", function() {
     var num1 = parseInt(this.id.match(/\d+/g), 10 );   // Obtener el número de seccion correspondiente
-    var referenciaCNT = "#referenciaCNT" + num1;       // Generar el identificador al contenedor de la referencia correspondiente
-
-    referenciaCount[num1-1]++;                         // Aumentar el contador de phones para el referencia permitente
-    var num2 = referenciaCount[num1-1];                // Variable auxiliar para la sustitucion en el html de abajo
-
-    autorCount[num1-1] = autorCount[num1-1].concat([1]);
-    var num3 = autorCount[num1-1][num2-1];
-
-    // Inserción del html
-    $(referenciaCNT).append(
-    '<hr>\
-    <div id="referencia'+num1+'-'+num2+'">\
-      <label><small>Referencia '+num1+'.'+num2+'</small></label>\
-      <input type="hidden" name="referenciapk'+num1+'-'+num2+'" value=-1>\
-      <input type="text" name="titulo'+num1+'-'+num2+'" placeholder="Título de la referencia">\
-      <div id="autorCNT'+num1+'-'+num2+'">\
-        <div id="autor'+num1+'-'+num2+'-'+num3+'">\
-          <label><small>Autor '+num1+'-'+num2+'-'+num3+'</small></label>\
-          <input type="hidden" name="autorpk'+num1+'-'+num2+'-'+num3+'" value=-1>\
-          <input type="text" name="nombres'+num1+'-'+num2+'-'+num3+'" placeholder="Nombres">\
-          <input type="text" name="apellidos'+num1+'-'+num2+'-'+num3+'" placeholder="Apellidos">\
-        </div>\
-      </div>\
-      <div align="right">\
-        <button id="agregarAutor'+num1+'-'+num2+'" class="agregarAutor" type="button" style="float: right;"><small>Agregar autor</small></button>\
-      </div>\
-      </br>\
-      </br>\
-      <label><small>Editorial</small></label>\
-      <input type="text" name="editorial'+num1+'-'+num2+'" placeholder="Editorial de la referencia">\
-      <label><small>Edición</small></label>\
-      <input type="text" name="edicion'+num1+'-'+num2+'" placeholder="Edición de la referencia">\
-      <label><small>Notas</small></label>\
-      <input type="text" name="notas'+num1+'-'+num2+'" placeholder="Notas de la referencia">\
-    </div>');
+    anadirUnaReferencia(num1);
   });
 
   // Función para los botones para añadir autor
   $("body").on("click","button.agregarAutor", function() {
-
-    
     var matchess = this.id.match(/\d+/g);
     num1 = matchess[0];
     num2 = matchess[1];
-    var autorCNT = "#autorCNT" + num1 + '-' + num2;
-    autorCount[num1-1][num2-1]++;
-    var num3 = autorCount[num1-1][num2-1];
-
-    $(autorCNT).append(
-      '<hr>\
-      <div id="autor'+num1+'-'+num2+'-'+num3+'">\
-      <label><small>Autor '+num1+'.'+num2+'.'+num3+'</small></label>\
-      <input type="hidden" name="autorpk'+num1+'-'+num2+'-'+num3+'" value=-1>\
-      <input type="text" name="nombres'+num1+'-'+num2+'-'+num3+'" placeholder="Nombres"">\
-      <input type="text" name="apellidos'+num1+'-'+num2+'-'+num3+'" placeholder="Apellidos"">\
-      </div>');
-    });
+    anadirUnAutor(num1, num2);  
+  });
 
   // Función para el botón para añadir secciones
   $(addSeccion).on('click', function() {
